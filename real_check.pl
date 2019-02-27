1add9616ea4934581ef8d8a0ea8d5512b08b0d8f
Jacob Wilkins <jacob.wilkins@oerc.ox.ac.uk>
2019-02-27 22:30:46 +0000

    Added real_check

---
+++ real_check.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env per
+# Real_check is a tool to check whether floats are followed by a kind specifier in Fortran
+# Written by J. Wilkins 2017
+
+$float=qr/((?>\d+\.(?>\d+)?(?>[eE][+ -]?\d+)?))/;
+$lop  =qr/(?:eq|ge|gt|le|lt|and|or|xor|neqv|eqv|not|neq)/;
+$i=0;
+while (<>) {
+    $i++;
+    if (/[^vViIbBoOzZfFeEnNsSgGdD0-9]$float(?!(?:$lop|_))/ && !/^\s*!/) {print $i,$_;}
+}
